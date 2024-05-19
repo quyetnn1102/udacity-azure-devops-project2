c2228892f3c4e7ef3bbcb8f12aaf36969c872149
Quyet Nguyen <quyetnn2051987@gmail.com>
2024-05-19 11:28:43 +0700

    demo 22

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home DEMO</h3>"
+    html = "<h3>Sklearn Prediction Home DEMO 2</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
