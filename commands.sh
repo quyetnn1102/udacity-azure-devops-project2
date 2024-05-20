0628d8d9354bc5cdff54efb504e4b76217df50c1
Quyet Nguyen <quyetnn2051987@gmail.com>
2024-05-20 09:46:30 +0700

    update comment review

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-az webapp up -n quyetnn-udacity-project2 -g Azuredevops --sku FREE
+git clone git@github.com:quyetnn1102/udacity-azure-devops-project2.git; cd udacity-azure-devops-project2; git pull; python3 -m venv ~/.udacity-azure-devops-project2; source ~/.udacity-azure-devops-project2/bin/activate; make all; az webapp up -n quyetnn-udacity-project2 -g Azuredevops --sku FREE
--- locustfile.py
+++ locustfile.py
@@ -3,10 +3,12 @@ from locust import HttpUser, task, between
 class WebsiteTestUser(HttpUser):
     wait_time = between(0.5, 3.0)
 
+    data = {"CHAS": {"0": 0}, "RM": {"0": 6.575}, "TAX": {"0": 296.0}, "PTRATIO": {"0": 15.3}, "B": {"0": 396.9},
+            "LSTAT": {"0": 4.98}}
     @task(1)
     def test1(self):
         self.client.get("https://quyetnn-udacity-project2.azurewebsites.net/")
 
     @task(2)
     def test2(self):
-        self.client.post("https://quyetnn-udacity-project2.azurewebsites.net:443/predict")
+        self.client.post("https://quyetnn-udacity-project2.azurewebsites.net:443/predict", json=self.data)
